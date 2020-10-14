83160b58193a72aa5a38bd3455c7e3a0da03c586
root <root@antix1>
2020-10-14 16:06:53 +0900

    modified:   Part.pde 	modified:   PartsImageList.pde 	modified:   build/source/ECAD.java

--- Part.pde
+++ Part.pde
@@ -1,66 +1,41 @@
-// class Part{
-// 	int sizeX,sizeY,posX,posY,deg,ofsetX,ofsetY,redPointX,redPointY;
-// 	boolean isNoMove=false;
-// 	PImage image;
-// 	Part(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY,PImage _image){
-// 		newParts(_deg,_posX,_posY,_sizeX,_sizeY,_ofsetX,_ofsetY,_image);
-// 	}
-// 	void newParts(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY,PImage _image) {
-// 		posX=_posX;
-// 		posY=_posY;
-// 		sizeX=_sizeX;
-// 		sizeY=_sizeY;
-// 		ofsetX=_ofsetX;
-// 		ofsetY=_ofsetY;
-//
-// 		image= _ image;
-//
-// 		if(_deg!=-1){
-// 			deg=_deg;
-// 		}
-// 	}
-//
-// 	void moveParts(int _posX,int _posY){
-// 		isNoMove=false;
-// 		if(posX==_posX&&posY==_posY){
-// 			isNoMove=true;
-// 		}
-// 		posX=_posX;
-// 		posY=_posY;
-// 	}
-//
-// 	boolean moveCheck(int _posX,int _posY){
-// 		isNoMove=false;
-// 		if(posX==_posX&&posY==_posY){
-// 			isNoMove=true;
-// 		}
-// 		return isNoMove;
-// 	}
-//
-// 	boolean posCheck(){
-// 		if (deg==0){
-// 			if((mouseX>=posX-ofsetX)&&(mouseY>=posY-ofsetY)&&(mouseX<=posX+sizeX-ofsetX)&&(mouseY<=posY+sizeY-ofsetY)){
-// 					return true;
-// 			}
-// 		}else if(deg==1){
-// 			if((mouseX>=posX-sizeY+ofsetY)&&(mouseY>=posY-ofsetX)&&(mouseX<=posX+ofsetY)&&(mouseY<=posY+sizeX-ofsetX)) {
-// 				return true;
-// 			}
-// 		}else if(deg==2){
-// 			if((mouseX>=posX-sizeX-ofsetX)&&(mouseY>=posY-sizeY+ofsetY)&&(mouseX<=posX+ofsetX)&&(mouseY<=posY+ofsetY)){
-// 				return true;
-// 			}
-// 		}else if(deg==3){
-// 			if((mouseX>=posX-ofsetY)&&(mouseY>=posY-sizeX+ofsetX)&&(mouseX<=posX-ofsetX+sizeY)&&(mouseY<=posY-ofsetY+sizeX)){
-// 				return true;
-// 			}
-// 		}
-// 		return false;
-// 	}
-//
-// 	void turn(int adeg){
-// 		if(adeg!=-1){
-// 			deg=adeg;
-// 		}
-// 	}
-// }
+class Part{
+	int sizeX,sizeY,posX,posY,deg,ofsetX,ofsetY,redPointX,redPointY;
+	boolean isNoMove=false;
+	Part(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY){
+		newParts(_deg,_posX,_posY,_sizeX,_sizeY,_ofsetX,_ofsetY);
+	}
+	void newParts(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY) {
+		posX=_posX;
+		posY=_posY;
+		sizeX=_sizeX;
+		sizeY=_sizeY;
+		ofsetX=_ofsetX;
+		ofsetY=_ofsetY;
+		if(_deg!=-1){
+			deg=_deg;
+		}
+	}
+
+	void moveParts(int _posX,int _posY){
+		isNoMove=false;
+		if(posX==_posX&&posY==_posY){
+			isNoMove=true;
+		}
+		posX=_posX;
+		posY=_posY;
+	}
+
+	boolean moveCheck(int _posX,int _posY){
+		isNoMove=false;
+		if(posX==_posX&&posY==_posY){
+			isNoMove=true;
+		}
+		return isNoMove;
+	}
+
+	void turn(int adeg){
+		if(adeg!=-1){
+			deg=adeg;
+		}
+	}
+}
--- build/source/ECAD.java
+++ build/source/ECAD.java
@@ -421,7 +421,11 @@ class ButtonGroup{
 	}
 }
 
-
+class Collision{
+	Collision(){
+		
+	}
+}
 class Drawing{
 	Drawing(){
 		
@@ -443,72 +447,47 @@ class Object{
 		
 	}
 }
-// class Part{
-// 	int sizeX,sizeY,posX,posY,deg,ofsetX,ofsetY,redPointX,redPointY;
-// 	boolean isNoMove=false;
-// 	PImage image;
-// 	Part(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY,PImage _image){
-// 		newParts(_deg,_posX,_posY,_sizeX,_sizeY,_ofsetX,_ofsetY,_image);
-// 	}
-// 	void newParts(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY,PImage _image) {
-// 		posX=_posX;
-// 		posY=_posY;
-// 		sizeX=_sizeX;
-// 		sizeY=_sizeY;
-// 		ofsetX=_ofsetX;
-// 		ofsetY=_ofsetY;
-//
-// 		image= _ image;
-//
-// 		if(_deg!=-1){
-// 			deg=_deg;
-// 		}
-// 	}
-//
-// 	void moveParts(int _posX,int _posY){
-// 		isNoMove=false;
-// 		if(posX==_posX&&posY==_posY){
-// 			isNoMove=true;
-// 		}
-// 		posX=_posX;
-// 		posY=_posY;
-// 	}
-//
-// 	boolean moveCheck(int _posX,int _posY){
-// 		isNoMove=false;
-// 		if(posX==_posX&&posY==_posY){
-// 			isNoMove=true;
-// 		}
-// 		return isNoMove;
-// 	}
-//
-// 	boolean posCheck(){
-// 		if (deg==0){
-// 			if((mouseX>=posX-ofsetX)&&(mouseY>=posY-ofsetY)&&(mouseX<=posX+sizeX-ofsetX)&&(mouseY<=posY+sizeY-ofsetY)){
-// 					return true;
-// 			}
-// 		}else if(deg==1){
-// 			if((mouseX>=posX-sizeY+ofsetY)&&(mouseY>=posY-ofsetX)&&(mouseX<=posX+ofsetY)&&(mouseY<=posY+sizeX-ofsetX)) {
-// 				return true;
-// 			}
-// 		}else if(deg==2){
-// 			if((mouseX>=posX-sizeX-ofsetX)&&(mouseY>=posY-sizeY+ofsetY)&&(mouseX<=posX+ofsetX)&&(mouseY<=posY+ofsetY)){
-// 				return true;
-// 			}
-// 		}else if(deg==3){
-// 			if((mouseX>=posX-ofsetY)&&(mouseY>=posY-sizeX+ofsetX)&&(mouseX<=posX-ofsetX+sizeY)&&(mouseY<=posY-ofsetY+sizeX)){
-// 				return true;
-// 			}
-// 		}
-// 		return false;
-// 	}
-//
-// 	void turn(int adeg){
-// 		if(adeg!=-1){
-// 			deg=adeg;
-// 		}
-// 	}
-// }
+class Part{
+	int sizeX,sizeY,posX,posY,deg,ofsetX,ofsetY,redPointX,redPointY;
+	boolean isNoMove=false;
+	Part(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY){
+		newParts(_deg,_posX,_posY,_sizeX,_sizeY,_ofsetX,_ofsetY);
+	}
+	public void newParts(int _deg,int _posX,int _posY,int _sizeX,int _sizeY,int _ofsetX,int _ofsetY) {
+		posX=_posX;
+		posY=_posY;
+		sizeX=_sizeX;
+		sizeY=_sizeY;
+		ofsetX=_ofsetX;
+		ofsetY=_ofsetY;
+		if(_deg!=-1){
+			deg=_deg;
+		}
+	}
+
+	public void moveParts(int _posX,int _posY){
+		isNoMove=false;
+		if(posX==_posX&&posY==_posY){
+			isNoMove=true;
+		}
+		posX=_posX;
+		posY=_posY;
+	}
+
+	public boolean moveCheck(int _posX,int _posY){
+		isNoMove=false;
+		if(posX==_posX&&posY==_posY){
+			isNoMove=true;
+		}
+		return isNoMove;
+	}
+
+	public void turn(int adeg){
+		if(adeg!=-1){
+			deg=adeg;
+		}
+	}
+}
 class PartList{
 	PartList(){
 
@@ -524,7 +503,7 @@ class PartListList{
 
 class PartImageList{
 	PartImageList(){
-		
+
 	}
 }
 class PartImageLoad{
